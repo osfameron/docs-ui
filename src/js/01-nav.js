16357a57466539d2dbb1527f75be7ddf48cb9f27
anmoltripathi <anmol.tripathi@algoworks.com>
2021-01-07 15:54:20 +0530

    update left navigation

--- src/js/01-nav.js
+++ src/js/01-nav.js
@@ -1,131 +1,97 @@
 ;(function () {
   'use strict'
 
-  var nav = document.querySelector('nav.nav')
-  var menuExpandToggle = document.querySelector('.menu-expand-toggle')
-  var navMenu = {}
-  if (!(navMenu.element = nav && nav.querySelector('.nav-menu'))) return
-  var navControl
-  // var currentPageItem = navMenu.element.querySelector('.is-current-page')
+  // expand nav to show current page
+  var currentPageItem = Array.from(document.querySelectorAll('.is-current-page')).pop().parentNode
+  console.log(currentPageItem)
+  expandParents(currentPageItem)
 
-  // NOTE prevent text from being selected by double click
-  navMenu.element.addEventListener('mousedown', function (e) {
-    if (e.detail > 1) e.preventDefault()
-  })
-
-  find(/*'.nav-toggle',*/ '.in-toggle', navMenu.element).forEach(function (toggleBtn) {
-    // console.log(toggleBtn, 17)
-    var navItem = findAncestorWithClass('nav-item', toggleBtn, navMenu.element)
-    toggleBtn.addEventListener('click', toggleActive.bind(navItem))
-    // var dataDepth = toggleBtn.getAttribute('data-depth')
-    // if (dataDepth === 1) {
-    //   toggleBtn.classList.add("mystyle")
-    // }
-    // toggleBtn.addEventListener('click', addActive.bind(navItem))
-
-    var navItemSpan = findNextElement(toggleBtn)
-    if (navItemSpan.classList.contains('nav-text')) {
-      navItemSpan.style.cursor = 'pointer'
-      navItemSpan.addEventListener('click', toggleActive.bind(navItem))
-      // navItemSpan.addEventListener('click', addActive.bind(navItem))
+  function expandParents (element) {
+    var panel = element.parentNode
+    console.log(panel)
+    if (!panel.matches('.nav-children-panel')) {
+      return
     }
-  })
-
-  if ((navControl = document.querySelector('main .nav-control'))) navControl.addEventListener('click', revealNav)
-
-  // Toggle class
-  function toggleActive (e) {
-    // this.classList.toggle('open')
-    this.classList.toggle('is-active')
+    var parentHeader = panel.previousElementSibling
+    console.log(parentHeader)
+    panel.classList.remove('hide')
+    parentHeader.querySelector('.material-icons').classList.add('expanded')
+    expandParents(parentHeader)
   }
 
-  function revealNav (e) {
-    if (nav.classList.contains('is-active')) return hideNav(e)
-    document.documentElement.classList.add('is-clipped--nav')
-    nav.classList.add('is-active')
-    nav.addEventListener('click', concealEvent)
-    window.addEventListener('click', hideNav)
-    concealEvent(e) // NOTE don't let event get picked up by window click listener
+  // persist version selections
+  var select = document.querySelectorAll('.select-version')
+  var versionVar = 'sdp-docs-versions'
+  var versions = getVersions()
+
+  if (!versions) {
+    versions = {}
+    for (var i = 0; i < select.length; i++) {
+      versions[select[i].getAttribute('data-component')] = 0
+    }
+    setVersions(versions)
+  } else {
+    Object.entries(versions).forEach(function (component) {
+      var componentName = component[0]
+      var componentVersionIndex = component[1]
+      var componentSelectVersion = document.querySelector('select[data-component="' + componentName + '"]')
+      componentSelectVersion.selectedIndex = componentVersionIndex
+      var componentVersion = componentSelectVersion.options[componentVersionIndex].value
+      setComponentVersion(componentName, componentVersion)
+    })
   }
 
-  function hideNav (e) {
-    if (e.which === 3 || e.button === 2) return
-    document.documentElement.classList.remove('is-clipped--nav')
-    nav.classList.remove('is-active')
-    nav.removeEventListener('click', concealEvent)
-    window.removeEventListener('click', hideNav)
-    concealEvent(e) // NOTE don't let event get picked up by window click listener
+  function setVersions (versions) {
+    return window.sessionStorage.setItem(versionVar, JSON.stringify(versions))
   }
 
-  function find (selector, from) {
-    return [].slice.call((from || document).querySelectorAll(selector))
+  function getVersions () {
+    return JSON.parse(window.sessionStorage.getItem(versionVar))
   }
 
-  function findAncestorWithClass (className, from, scope) {
-    if ((from = from.parentNode) !== scope) {
-      if (from.classList.contains(className)) {
-        return from
+  function setComponentVersion (component, version) {
+    var showSelector = '.nav-container div[data-component="' + component + '"][data-version="' + version + '"]'
+    var hideSelector = '.nav-container div[data-component="' + component + '"]:not(.hide)'
+    var navShow = document.querySelector(showSelector)
+    var navHide = document.querySelector(hideSelector)
+    navHide.classList.add('hide')
+    navShow.classList.remove('hide')
+  }
+
+  for (var j = 0; j < select.length; j++) {
+    var s = select[j]
+    s.addEventListener('change', function (event) {
+      var component = this.getAttribute('data-component')
+      var versionIndex = this.selectedIndex
+      var version = this.options[versionIndex].value
+      var versions = getVersions()
+      versions[component] = versionIndex
+      setVersions(versions)
+      setComponentVersion(component, version)
+    })
+
+    // Disable select if there is only one version
+    if (s.options.length === 1) {
+      s.classList.add('single-version')
+      s.disabled = true
+    }
+  }
+
+  /// nav-tree
+  var x = document.querySelectorAll('.nav-item:not(.is-link), .nav-item.is-link .material-icons')
+  for (var k = 0; k < x.length; k++) {
+    // mdc.ripple.MDCRipple.attachTo(x[k])
+    x[k].addEventListener('click', function (event) {
+      var target = event.target
+      var item = target.matches('span') ? target.previousElementSibling : target
+      var panel = item.parentElement.nextElementSibling
+      if (item.classList.contains('expanded')) {
+        item.classList.remove('expanded')
+        panel.classList.add('hide')
       } else {
-        return findAncestorWithClass(className, from, scope)
-      }
-    }
-  }
-
-  function findNextElement (from, el) {
-    if ((el = from.nextElementSibling)) return el
-    el = from
-    while ((el = el.nextSibling) && el.nodeType !== 1);
-    return el
-  }
-
-  menuExpandToggle.addEventListener('click', function (e) {
-    e.preventDefault()
-    if (nav.classList.contains('collapse-menu')) {
-      nav.classList.remove('collapse-menu')
-    } else {
-      nav.classList.add('collapse-menu')
-    }
-  })
-  // has children in li
-  function concealEvent (e) {
-    e.stopPropagation()
-  }
-  // scroll left menu to current active page
-  setTimeout(function () {
-    if (document.querySelector('.is-current-page')) {
-      var currentPageMenu = document.querySelector('.is-current-page')
-      var topPositon = currentPageMenu.offsetTop - 100
-      var leftMenu = document.querySelector('.left-sidebar-menu .nav-menu')
-      leftMenu.scrollTop = topPositon
-    }
-  }, 100) //setTime Out end
-
-  // show depth 0 child element
-  if (document.querySelector('.is-current-page')) {
-    var otherNavs = document.querySelectorAll('.nav-list > .nav-item[data-depth="0"]')
-    otherNavs.forEach(function (nav) {
-      var navSubMenu = Array.from(nav.querySelector('ul.nav-list').children)
-      // var navDataDepth = Array.from(nav.querySelector('ul.nav-list'))
-      navSubMenu.forEach(function (item) {
-        item.classList.remove('is-inactive')
-      })
-
-      // hide main menu for top level navigation -
-
-      // if (nav.className.includes('is-current-page')) {
-      //   navMenuControl.style.display = 'none'
-      // }
-
-      // hide in second level menu
-      if (nav.className.includes('is-current-path')) {
-        otherNavs.forEach(function (navItem) {
-          if (!navItem.className.includes('is-current-path')) {
-            navItem.classList.add('is-inactive')
-          }
-        })
+        item.classList.add('expanded')
+        panel.classList.remove('hide')
       }
     })
-  } // if condition end
-
-  // clearTimeout(scrollCurrentPageMenu, 20000)
+  }
 })()
