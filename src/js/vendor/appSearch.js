b55a887318dec677fdebe29325da97a52f0c22ac
anmoltripathi <anmol.tripathi@algoworks.com>
2021-02-05 22:03:12 +0530

    update search functionality

--- src/js/vendor/appSearch.js
+++ src/js/vendor/appSearch.js
@@ -1,5 +1,4 @@
 /* global instantsearch algoliasearch */
-var customSearch = {}
 var search = instantsearch({
   indexName: 'test_stage_docs_couchbase', //prod_docs_couchbase',
   searchClient: algoliasearch('NI1G57N08Q', 'd3eff3e8bcc0860b8ceae87360a47d54'),
@@ -10,26 +9,43 @@ var search = instantsearch({
       return
     } else {
       searchResults.style.display = 'block'
+      console.log(helper.state.query)
+      document.getElementById('queryText').innerHTML = helper.state.query
     }
 
     helper.search()
   },
 })
+var customSearch = {}
 
 search.addWidgets([
+  instantsearch.widgets.configure({
+    hitsPerPage: 40,
+    // enablePersonalization: true,
+    // attributeForDistinct: 'hierarchy.lvl0',
+    // distint: true,
+  }),
+
   instantsearch.widgets.searchBox({
     container: '#searchbox',
-    placeholder: 'Search for products...',
+    placeholder: 'Search Docs',
     autofocus: false,
-    // searchAsYouType: false,
-    showSubmit: false,
-    showReset: false,
     showLoadingIndicator: false,
+    searchAsYouType: true,
+    showSubmit: true,
+    showReset: false,
     searchOnInit: false,
-    // attributeForDistinct: 'hierarchy.lvl0',
-    distint: true,
     facets: ['*'],
   }),
+
+  // instantsearch.widgets.panel({
+  //   hidden: function (options) {
+  //     return options.results.nbHits === 0
+  //   },
+  //   templates: {
+  //     header: 'Brand',
+  //   },
+  // }),
 
   instantsearch.widgets.refinementList({
     templates: {
@@ -48,7 +64,7 @@ search.addWidgets([
   }),
 
   instantsearch.widgets.hits({
-    container: '#hits-container',
+    container: '#hits',
     templates: {
       // eslint-disable-next-line max-len
       item: '<div class="search-result"><a href="{{url}}" class="search-title">{{{_highlightResult.hierarchy.lvl1.value}}}</a><div class="search-content">{{content}}</div><div class="search-meta"><span>{{component}}</span><span>{{}}</span><span>Version({{version}})</span></div></div>',
@@ -68,13 +84,17 @@ search.addWidgets([
         // console.log(val[0], val[1], 70)
         itemsArr.push({ name: val[0], value: val[1] })
       })
-      console.log(itemsArr, 74)
+      // console.log(itemsArr, 74)
       return items
     },
   }),
 
   instantsearch.widgets.pagination({
-    container: '#pagination-container',
+    container: '#pagination',
+  }),
+
+  instantsearch.widgets.stats({
+    container: '#stats',
   }),
 
 ])
