6c0bf1cdf23c0b3e82d95885d8293a1d1017324f
Dan Allen <dan@opendevise.com>
2021-02-10 02:14:38 -0700

    continue assembling nav

--- src/helpers/nav-group.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = ({ data: { root: { page, site } } }) => {
-  const pageComponentName = page.component.name
-  return site.keys.navGroups.find(({ components }) => components.includes(pageComponentName))
-}
